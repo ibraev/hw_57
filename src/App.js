124f0e6ef05253c5c6f4ba6af660fd54796acb3a
Ibraev Iskender <amanibraev95@gmail.com>
2019-01-11 02:23:21 +0600

    #4. Added data about items.

--- src/App.js
+++ src/App.js
@@ -4,21 +4,17 @@ import Items from './components/Additems'
 
 class App extends Component {
 
-  state = {
-    foods : [
-        {name: 'Milk', count: 0, price: 40},
-        {name: 'Bread', count: 0, price: 15},
-        {name: 'Dinner at cafe', count: 0, price: 400},
-        {name: 'Bottle of water', count: 0, price: 25}
-    ],
-
-      total: 0,
-  };
+    state = {
+        purchases: [],
+        title: '',
+        cost: 0,
+        total: 0,
+    };
 
   render() {
     return (
       <div className="App">
-        <Items></Items>
+
       </div>
     );
   }
