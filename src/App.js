22530fd76b5bfcf57a9d7dbec4478e988699044a
Ibraev Iskender <amanibraev95@gmail.com>
2019-01-10 13:57:39 +0600

    #1. Started to work.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    foods : [
+        {name: 'Milk', count: 0, price: 40},
+        {name: 'Bread', count: 0, price: 15},
+        {name: 'Dinner at cafe', count: 0, price: 400},
+        {name: 'Bottle of water', count: 0, price: 25}
+    ],
+
+      total: 0,
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
