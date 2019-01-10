bf34dbeb2d6dad1897ec6f08d088e16ab13fcfb3
Ibraev Iskender <amanibraev95@gmail.com>
2019-01-11 00:55:33 +0600

    #2. Created scripts

--- home_work_57-ts-1/task-1.js
+++ home_work_57-ts-1/task-1.js
@@ -1,4 +1,6 @@
+
 const tasks = [
+
     {id: 234, title: 'Create user registration API', timeSpent: 4, category: 'Backend', type: 'task'},
     {id: 235, title: 'Create user registration UI', timeSpent: 8, category: 'Frontend', type: 'task'},
     {id: 237, title: 'User sign-in via Google UI', timeSpent: 3.5, category: 'Frontend', type: 'task'},
@@ -6,17 +8,54 @@ const tasks = [
     {id: 241, title: 'Fix account linking', timeSpent: 5, category: 'Backend', type: 'bug'},
     {id: 250, title: 'Fix wrong time created on new record', timeSpent: 1, category: 'Backend', type: 'bug'},
     {id: 262, title: 'Fix sign-in failed messages', timeSpent: 2, category: 'Frontend', type: 'bug'},
+
 ];
 
-const totalTasks = tasks.reduce((acc, item) => {
-    const front = item.category;
+const front = [];
+const typeBugs = [];
+const findWords = [];
 
-    if (front in acc) {
-        acc[front]++;
-    }else {
-        acc[front] = 1;
+let objects = {
+    Frontend: 0,
+    Backend: 0,
+};
+const newTasks = [];
+
+
+
+tasks.map((task => {
+    if (task.category === 'Frontend') {
+        front.push(task.timeSpent);
+        objects.Frontend++
     }
-    return acc + item.timeSpent;
-}, 0);
+    if (task.type === 'bug') {
+        typeBugs.push(task.timeSpent)
+    }
+    if (task.title.indexOf('UI') !== -1) {
+        findWords.push(task)
+    }
+    if (task.category === 'Backend') {
+        objects.Backend++
+    }
+    if (task.timeSpent > 4) {
+        let tasksObject = {
+            title: task.title,
+            category: task.category,
+        };
+        newTasks.push(tasksObject)
+    }
 
-console.log('Total amount of time, work on tasks from the Frontend category', totalTasks + ' min');
+
+}));
+
+const UI = tasks.filter(item => item.title.includes("UI"));
+
+
+console.log('The total amount of time spent on work on tasks from the \'Frontend\' category = ' + front.reduce((accumulator, currentValue) => accumulator + currentValue));
+console.log('The total amount of time spent on work on tasks like \'bug\'. = ' + typeBugs.reduce((accumulator, currentValue) => accumulator + currentValue));
+console.log('The number of tasks that have the word "UI" in the title. = ' + UI.length);
+console.log(objects);
+console.log(newTasks);
+
+
+
