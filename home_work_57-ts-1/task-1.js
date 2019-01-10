cb44f9804e35c384317dfee9360d669479941ca3
Ibraev Iskender <amanibraev95@gmail.com>
2019-01-10 17:31:48 +0600

    #2. Created task-1

---
+++ home_work_57-ts-1/task-1.js
@@ -0,0 +1,22 @@
+const tasks = [
+    {id: 234, title: 'Create user registration API', timeSpent: 4, category: 'Backend', type: 'task'},
+    {id: 235, title: 'Create user registration UI', timeSpent: 8, category: 'Frontend', type: 'task'},
+    {id: 237, title: 'User sign-in via Google UI', timeSpent: 3.5, category: 'Frontend', type: 'task'},
+    {id: 238, title: 'User sign-in via Google API', timeSpent: 5, category: 'Backend', type: 'task'},
+    {id: 241, title: 'Fix account linking', timeSpent: 5, category: 'Backend', type: 'bug'},
+    {id: 250, title: 'Fix wrong time created on new record', timeSpent: 1, category: 'Backend', type: 'bug'},
+    {id: 262, title: 'Fix sign-in failed messages', timeSpent: 2, category: 'Frontend', type: 'bug'},
+];
+
+const totalTasks = tasks.reduce((acc, item) => {
+    const front = item.category;
+
+    if (front in acc) {
+        acc[front]++;
+    }else {
+        acc[front] = 1;
+    }
+    return acc + item.timeSpent;
+}, 0);
+
+console.log('Total amount of time, work on tasks from the Frontend category', totalTasks + ' min');
